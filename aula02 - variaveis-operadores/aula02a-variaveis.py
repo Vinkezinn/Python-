112e82ef9182129c0d238df7d0ecd78893339e7c
Vinkezinn <fiapinho333@gmail.com>
2026-03-17 11:42:40 -0300

    aula 2

---
+++ aula02 - variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,27 @@
+print("ola mundo")
+
+print(7+4)
+print("7+4")
+print("7" + "4")
+
+# Comentarios de 1 linha
+
+'''
+multilplas linhas
+
+'''
+
+#variaveis
+nome="kevin"
+idade= "17"
+peso= 65
+
+print(nome, idade, peso )
+print(f"oiii, {nome}!")
+
+#input -- simula Forms no cmd
+nome = input("digite seu nome")
+idade = int(input("digite sua idade"))
+peso = float(input("digite seu peso"))
+
+print(nome, idade, peso)
---
+++ aula02 - variaveis-operadores/desafia aula 2a.py
@@ -0,0 +1,12 @@
+nome = input("digite seu nome")
+print(f"Seja bem vindo, {nome}!!")
+
+ano = input("ano de nascimento")
+mes = input("mes de nascimento")
+dia = input("dia de nascimento")
+print(f"{dia}/{mes}/{ano}")
+
+
+num1 = int(input("digite um numero:"))
+num2 = int(input("digite outro numero"))
+print(num1+num2)
